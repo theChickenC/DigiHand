45ed4e847c3664b89661f8c9a3372db9d566c09e
jockey <jockeyshred@gmail.com>
2026-09-22 20:25:35 -0400

    buttons for pdf controller

--- pdfController.py
+++ pdfController.py
@@ -1,9 +1,15 @@
 import constants
 import utils
-from PySide6.QtGui import QFont, QImage, QTextDocument
+import os
+import sys
+
+from PySide6.QtGui import QAction, QActionGroup, QFont, QIcon, QKeySequence, QImage, QTextDocument
+from PySide6.QtCore import QSize, Qt, Signal, Slot
 from PySide6.QtPdfWidgets import QPdfView
 from PySide6.QtPdf import QPdfDocument
-from pdfView import PdfView
+from wordView import WordView
+from widgets import CustomTextEdit
+
 from PySide6.QtWidgets import (
     QApplication,
     QComboBox,
@@ -17,6 +23,10 @@ from PySide6.QtWidgets import (
     QHBoxLayout,
     QWidget,
     QPushButton,
+    QSpacerItem,
+    QSizePolicy,
+    QSpinBox,
+    QAbstractSpinBox,
 )
 
 
@@ -27,9 +37,53 @@ class PdfController(QWidget):
         self.layout = QHBoxLayout(self)
         self.layout.setContentsMargins(0,0,0,0)
         self.layout.setSpacing(0)
-        self.dummyButton = QPushButton("Dummy Button")
-        self.layout.addWidget(self.dummyButton)
-        
+
+        self.pbZoomIn = QPushButton(self)
+        self.pbZoomIn.setIcon(QIcon(os.path.join("images", "zoom-in.svg")))
+        self.pbZoomIn.setStatusTip("Zoom In PDF")
+        self.layout.addWidget(self.pbZoomIn)
+
+        self.pbZoomOut = QPushButton(self)
+        self.pbZoomOut.setIcon(QIcon(os.path.join("images", "zoom-out.svg")))
+        self.pbZoomOut.setStatusTip("Zoom Out PDF")
+        self.layout.addWidget(self.pbZoomOut)
+
+        self.pbZoomFit = QPushButton(self)
+        self.pbZoomFit.setIcon(QIcon(os.path.join("images", "zoom-fit-best.svg")))
+        self.pbZoomFit.setStatusTip("Best Fit PDF Zoom")
+        self.layout.addWidget(self.pbZoomFit)
+
+        self.pbZoomOG = QPushButton(self)
+        self.pbZoomOG.setIcon(QIcon(os.path.join("images", "zoom-original.svg")))
+        self.pbZoomOG.setStatusTip("Original PDF Zoom")
+        self.layout.addWidget(self.pbZoomOG)
+
+        self.pbRotateL = QPushButton(self)
+        self.pbRotateL.setIcon(QIcon(os.path.join("images", "rotate-left.svg")))
+        self.pbRotateL.setStatusTip("Rotate PDF Left")
+        self.layout.addWidget(self.pbRotateL)
+
+        self.pbRotateR = QPushButton(self)
+        self.pbRotateR.setIcon(QIcon(os.path.join("images", "rotate-right.svg")))
+        self.pbRotateR.setStatusTip("Rotate PDF Right")
+        self.layout.addWidget(self.pbRotateR)
+
+        self.pbPrevPage = QPushButton(self)
+        self.pbPrevPage.setIcon(QIcon(os.path.join("images", "go-previous-view-page.svg")))
+        self.pbPrevPage.setStatusTip("Previous Page")
+        self.layout.addWidget(self.pbPrevPage)
+
+        self.curPage = QSpinBox(self)
+        self.curPage.setButtonSymbols(QAbstractSpinBox.NoButtons)
+        self.layout.addWidget(self.curPage)
+
+        self.pbNextPage = QPushButton(self)
+        self.pbNextPage.setIcon(QIcon(os.path.join("images", "go-next-view-page.svg")))
+        self.pbNextPage.setStatusTip("Next Page")
+        self.layout.addWidget(self.pbNextPage)
+
+        spacer = QSpacerItem(200, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self.layout.addSpacerItem(spacer)
 
     def canInsertFromMimeData(self, source):
         if source.hasImage():
